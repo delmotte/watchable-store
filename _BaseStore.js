2194f1a87f59b745ffa2b5eca0fe21b087ecbea8
Jean-Yves DELMOTTE <delmotte@polytech.unice.fr>
2016-10-11 15:33:36 +0200

    Update _BaseStore.js

--- _BaseStore.js
+++ _BaseStore.js
@@ -11,7 +11,8 @@ function deepFreeze (o) {
   Object.freeze(o);
 
   Object.getOwnPropertyNames(o).forEach(function (prop) {
-    if (o[prop] !== null
+    if (o.hasOwnProperty(prop)
+      && o[prop] !== null
     && (typeof o[prop] === "object" || typeof o[prop] === "function")
     && !Object.isFrozen(o[prop])) {
       deepFreeze(o[prop]);
